bcdde44411f570a9bfe0deeb5fe01ffcbc750a8e
c0b23161ae@edu.teu.ac.jp <c0b23161ae@edu.teu.ac.jp>
2024-09-24 18:22:06 +0900

    練習問題8

--- pygame_image.py
+++ pygame_image.py
@@ -13,20 +13,30 @@ def main():
     bg_img2= pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")  # 練習２
     kk_img = pg.transform.flip(kk_img, True, False)  # 練習２
+    kk_rct = kk_img.get_rect()  # 練習8-1：SurfaceからRectを抽出する
+    kk_rct.center = 300, 200  # 練習8-2：Rectを使った初期座標の設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%800)
+        key_lst = pg.key.get_pressed()  # 練習8-3：キーの押下状態を取得
+        if key_lst[pg.K_UP]:  # 上矢印キーがTrueなら
+            kk_rct.move_ip((0, -1))  # こうかとんの縦座標を-1する
+        if key_lst[pg.K_DOWN]:  # 下矢印キーがTrueなら
+            kk_rct.move_ip((0, +1))  # こうかとんの縦座標を+1する
+        if key_lst[pg.K_LEFT]:  # 下矢印キーがTrueなら
+            kk_rct.move_ip((-1, 0))  # こうかとんの横座標を-1する
+        if key_lst[pg.K_RIGHT]:  # 下矢印キーがTrueなら
+            kk_rct.move_ip((+1, 0))  # こうかとんの横座標を+1する
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
         screen.blit(kk_img, [300, 200])  # 練習４
+        screen.blit(kk_img, kk_rct)  # 練習４ -> 練習8-5
         pg.display.update()
         tmr += 1        
-        clock.tick(200)  # 練習５
         clock.tick(400)  # 練習５
 
 
